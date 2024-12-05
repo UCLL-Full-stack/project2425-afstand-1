d73998b3b08f87eb25735ce2080db455a12fbae1
kimberlyaustria <kimberly.austria@student.ucll.be>
2024-12-05 15:59:22 +0100

    isbn to string

--- back-end/test/model/bookclub.test.ts
+++ back-end/test/model/bookclub.test.ts
@@ -101,7 +101,7 @@ test('given: an existing user, when: adding a user to bookclub, then: user becom
 
 test('given: an existing book, when: adding a book to bookclub, then: book is added to the booklcub', () => {
     // given
-    const book = new Book( { isbn: 9780141439570 , title: 'The Picture Of Dorian Gray', author: 'Oscar Wilde', year: 2003 , pages: 304 , description: '**The Picture of Dorian Gray** by Oscar Wilde is a gothic masterpiece that explores themes of beauty, morality, and corruption. The story follows Dorian Gray, a handsome and vain young man, who wishes that his portrait would age in his place. His wish is mysteriously granted, allowing him to pursue a hedonistic lifestyle without visible consequences. As Dorian delves deeper into debauchery, his portrait grotesquely reflects the moral decay of his soul. Wilde’s provocative tale is a haunting examination of vanity, the price of indulgence, and the duality of human nature.' })
+    const book = new Book( { isbn: '9780141439570' , title: 'The Picture Of Dorian Gray', author: 'Oscar Wilde', year: 2003 , pages: 304 , description: '**The Picture of Dorian Gray** by Oscar Wilde is a gothic masterpiece that explores themes of beauty, morality, and corruption. The story follows Dorian Gray, a handsome and vain young man, who wishes that his portrait would age in his place. His wish is mysteriously granted, allowing him to pursue a hedonistic lifestyle without visible consequences. As Dorian delves deeper into debauchery, his portrait grotesquely reflects the moral decay of his soul. Wilde’s provocative tale is a haunting examination of vanity, the price of indulgence, and the duality of human nature.' })
     const bookclub = new Bookclub({ creator, name, description, books: [] })
 
     // when 
